f01867bde598502a3b2d8590d76f3871ff8e4b8d
andrealeo83 <andrealeo@libero.it>
2016-06-09 19:20:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  push: {
+      ios: {
+        pfx: ' __dirname + '/cloud/a.p12',
+        passphrase: '', // optional password to your p12/PFX
+        bundleId: 'it.frontiere21.Rukku',
+        production: true
+      }
+    }
+    
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
