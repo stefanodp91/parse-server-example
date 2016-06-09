f5842eb8db03ed7d981432879c7e4df9aadefb0d
andrealeo83 <andrealeo@libero.it>
2016-06-09 12:21:53 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -66,7 +66,7 @@ function getRequestDetail(idListForms){
 	query.equalTo("objectId", idListForms);
 	query.include('idUserRequest');
 	var myres = query.first();
-	console.log("getRequestDetail: " + dump(myres));
+	console.log("getRequestDetail: " + JSON.stringfy(myres));
 	return myres;
 }
 
@@ -78,7 +78,7 @@ function getOfferDetail(idListOffers){
 		query.equalTo("objectId", idListOffers);
 		query.include('idUserResponder');
 		var myres = query.first();
-		console.log("getOfferDetail: " + dump(myres));
+		console.log("getOfferDetail: " + JSON.stringfy(myres));
 		return myres;
 	}
 	return;
