a2bbc799c049049b85d7206c787cca5a45fee68c
GiuseppeF21 <giuseppe.trazza@frontiere21.it>
2016-06-20 15:36:39 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1170,7 +1170,7 @@ Parse.Cloud.define('testquery3', function(req, res) {
 Parse.Cloud.define('testnotify', function(req, res) {
 	console.log("testnotify");
 	var userQuery = new Parse.Query(Parse.User);
-	userQuery.equalTo("username", "lol");
+	userQuery.equalTo("username", "Peppe");
 
 	// Find devices associated with these users
 	var pushQuery = new Parse.Query(Parse.Installation);
