8554d72c1b57fa51a5b9935fa64342c0308750a4
GiuseppeF21 <giuseppe.trazza@frontiere21.it>
2016-09-01 12:42:04 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1755,6 +1755,9 @@ function retriveFile(url, callback){
 	  // The file contents are in response.buffer.
 	  console.log("retriveFile SUCCESS");
 	  console.log(response);
+	  console.log("status: response.status");
+	  console.log("status: response.HTTPResponse.status");
+	  console.log("type:");
 	  console.log(response.headers.content-type);
 	  console.log(response.buffer.data);
 	  //console.log(JSON.stringify(response));
