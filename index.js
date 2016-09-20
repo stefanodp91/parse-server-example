73c4b5bdbf50c5c01f633006c204165b748358da
andrealeo83 <andrealeo@libero.it>
2016-09-20 16:34:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ var api = new ParseServer({
     },
       ios: {
         //pfx: __dirname + '/cloud/a.p12',
-        pfx: __dirname + '/cloud/c.p12',
+        pfx: __dirname + '/cloud/RukkuCertificatoSviluppo.p12',
         passphrase: '', // optional password to your p12/PFX
         bundleId: 'it.frontiere21.Rukku',
-        production: true
+        //production: true
+        production: false
       }
     }
     
